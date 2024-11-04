8b20e183e60f883c3ba7f326942e02dda2e38568
PHAP <106916730+phapscac@users.noreply.github.com>
2024-11-04 12:30:26 +0700

    phap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { ListProductComponent } from './Product/list-product/list-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule,
