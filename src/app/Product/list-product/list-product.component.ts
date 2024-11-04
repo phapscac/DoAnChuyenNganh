eda206f323efeb0db1e53a19935cde6fc526ef04
PHAP <106916730+phapscac@users.noreply.github.com>
2024-11-04 12:22:08 +0700

    phap

--- src/app/Product/list-product/list-product.component.ts
+++ src/app/Product/list-product/list-product.component.ts
@@ -11,5 +11,9 @@ export class ListProductComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  products = [
+    { id: 1, name: 'Product 1', image: 'path/to/image1.jpg' },
+    { id: 2, name: 'Product 2', image: 'path/to/image2.jpg' },
+    { id: 3, name: 'Product 3', image: 'path/to/image3.jpg' }
+  ];
 }
