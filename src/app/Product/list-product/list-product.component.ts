4c67bc429742e627b4b1e0c230d8e8fdba197baf
PHAP <106916730+phapscac@users.noreply.github.com>
2024-12-09 11:54:21 +0700

    phap

--- src/app/Product/list-product/list-product.component.ts
+++ src/app/Product/list-product/list-product.component.ts
@@ -41,14 +41,14 @@ export class ListProductComponent implements OnInit {
      
    }
   services = [
-    {id: 1, name: 'Dich vu 1', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'},
-    {id: 2, name: 'Dich vu 2', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'},
-    {id: 3, name: 'Dich vu 3', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'},
-    {id: 4, name: 'Dich vu 4', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'},
-    {id: 5, name: 'Dich vu 5', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'},
-    {id: 6, name: 'Dich vu 6', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'},
-    {id: 7, name: 'Dich vu 7', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'},
-    {id: 8, name: 'Dich vu 8', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'}
+    {id: 1, name: 'Lắp đặt tủ điện', image: 'https://i.imgur.com/OorI6da.jpeg'},
+    {id: 2, name: 'Lắp đặt cầu trục', image: 'https://www.cokhicongnghiep.com/assets/upload/cokhicongnghiep.com/res/user-store/20028_thiet-bi/15284087-371735519884884-116142256476214452-n.jpg'},
+    {id: 3, name: 'Lắp đặt biến tần cho máy nén', image: 'https://i.imgur.com/RO6P98Z.jpeg'},
+    {id: 4, name: 'Lắp đặt tủ biến tần và máy biến cao áp ', image: 'https://i.imgur.com/2mXrlav.jpeg'},
+    {id: 5, name: 'Lắp đặt tủ điện cho hệ thống xếp thùng phi tại nhà máy dầu nhớt BP', image: 'https://i.imgur.com/semQFZ8.jpeg'},
+    {id: 6, name: 'Sửa chữa palang cho công trình điện gió', image: 'https://suadiencongnghiep.vn/upload/filemanager/files/sua-dien-tu-bu-cong-suat-phan-khang-kcn-tan-do.jpg'},
+    {id: 7, name: 'Sửa chữa biến tần công suất nhỏ và lớn', image: 'https://i.imgur.com/DCcatDm.jpeg'},
+    {id: 8, name: 'Lắp tủ điện điều khiển biến tần cho máy ép nhiệt ', image: 'https://i.imgur.com/7jb5Oej.jpeg'}
 
   ]
 }
